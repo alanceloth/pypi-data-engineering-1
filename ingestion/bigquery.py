024bef6be92b758b182d8f3e6862fccc22332d82
Alan Lanceloth <alan.lanceloth@gmail.com>
2024-08-07 22:18:51 -0300

    build: bigquery.py

---
+++ ingestion/bigquery.py
@@ -0,0 +1,46 @@
+from google.cloud import bigquery
+from google.oauth2 import service_account
+from google.auth.exceptions import DefaultCredentialsError
+import pandas as pd
+import time
+from loguru import logger
+import os
+
+def get_bigquery_client(project_name: str) -> bigquery.Client:
+    """Get BigQuery Client"""
+
+    try:
+        service_account_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+
+        if service_account_path:
+            credentials = service_account.Credentials.from_service_account_file(
+                service_account_path
+            )
+            bigquery_client = bigquery.Client(
+                project=project_name, credentials=credentials
+            )
+            return bigquery_client
+        
+        raise EnvironmentError(
+            "No valid credentials found for BigQuery auth"
+        )
+    except DefaultCredentialsError as creds_error:
+        raise creds_error
+    
+
+def get_bigquery_result(
+        query_str: str, bigquery_client: bigquery.Client
+) -> pd.DataFrame:
+    """Get query result from BigQuery and yield rows as dictionaries."""
+
+    try:
+        start_time = time.time()
+        logger.info(f"Running query: {query_str}")        
+        dataframe = bigquery_client.query(query_str).to_dataframe()
+        elapsed_time = time.time() - start_time
+        logger.info(f"Query executed and data loaded in {elapsed_time:.2f} seconds")
+        return dataframe
+    
+    except Exception as e:
+        logger.error(f"Error running query: {e}")
+        raise
